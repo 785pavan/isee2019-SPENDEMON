3bb92eb67122f6fb18578898656bc042c3b2816f
Pavan Kumar Kandapagari <40955141+785pavan@users.noreply.github.com>
2019-05-04 18:06:54 +0200

    xml

---
+++ app/src/main/java/com/dbse/android/spendemon/Signin_SignUp.java
@@ -0,0 +1,13 @@
+package com.dbse.android.spendemon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signin_SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin__sign_up);
+    }
+}
