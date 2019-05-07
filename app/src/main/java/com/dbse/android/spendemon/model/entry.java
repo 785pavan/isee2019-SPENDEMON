4a9bc5997d4a226eafc5bb67249acd0968c0d27c
Pavan Kumar Kandapagari <40955141+785pavan@users.noreply.github.com>
2019-05-07 18:39:37 +0200

    entry class added

---
+++ app/src/main/java/com/dbse/android/spendemon/model/entry.java
@@ -0,0 +1,40 @@
+package com.dbse.android.spendemon.model;
+
+import java.util.Date;
+
+public class entry {
+
+    private String category;
+    private float Amount;
+    private Date date;
+
+    public entry(String category, float amount, Date date) {
+        this.category = category;
+        this.Amount = amount;
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(float amount) {
+        Amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
