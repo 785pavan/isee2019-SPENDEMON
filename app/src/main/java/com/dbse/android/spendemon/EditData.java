aedd5c02481ad1e74d47f634da2022d450d8cde3
Pavan Kumar Kandapagari <40955141+785pavan@users.noreply.github.com>
2019-05-09 23:31:15 +0200

    Read and write Json added

--- app/src/main/java/com/dbse/android/spendemon/EditData.java
+++ app/src/main/java/com/dbse/android/spendemon/EditData.java
@@ -7,10 +7,21 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import com.dbse.android.spendemon.model.entry;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class EditData extends AppCompatActivity {
 
@@ -18,6 +29,7 @@ public class EditData extends AppCompatActivity {
     private Spinner sCategory;
     private EditText etAmount;
     private EditText etdate;
+    private static final String FILE_NAME = "data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,56 @@ public class EditData extends AppCompatActivity {
             entry.setDate(new SimpleDateFormat("dd/mm/yyyy").parse(etdate.getText().toString()));
         } catch (ParseException e) {
             Toast.makeText(getApplicationContext(),"Date format wrong",Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+    private void writeToJson(){
+        File file = new File(this.getFilesDir(), FILE_NAME);
+        FileReader fileReader = null;
+        FileWriter fileWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+
+        String response = null;
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+                fileWriter = new FileWriter(file.getAbsoluteFile());
+                bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write("{}");
+                bufferedWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        StringBuilder output = new StringBuilder();
+        try {
+            fileReader = new FileReader(file.getAbsoluteFile());
+            bufferedReader = new BufferedReader(fileReader);
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            response = output.toString();
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            JSONObject messageDetails = new JSONObject(response);
+            boolean isUserExisting = messageDetails.has("Username");
+            if (!isUserExisting) {
+                JSONArray newUserMessage = new JSONArray();
+                int id = (int) (Math.random() * 100);
+                newUserMessage.put(id);
+                messageDetails.put("Username", newUserMessage);
+            }
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
--- app/src/main/java/com/dbse/android/spendemon/Summary.java
+++ app/src/main/java/com/dbse/android/spendemon/Summary.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 import com.dbse.android.spendemon.model.entry;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,13 +12,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
-import android.widget.Spinner;
-import android.widget.TabWidget;
-import android.widget.TableLayout;
-import android.widget.TextView;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class Summary extends AppCompatActivity {
@@ -28,10 +36,10 @@ public class Summary extends AppCompatActivity {
     TableLayout tableLayout;*/
 
 
-
     private ArrayList<entry> entries;
     private final String INCOME = "income";
     private final String EXPENSE = "expense";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,12 +54,6 @@ public class Summary extends AppCompatActivity {
         rvEntries.setAdapter(adaptor);
         rvEntries.setLayoutManager(new LinearLayoutManager(this));
 
-        /*tableLayout = findViewById(R.id.tableLayout);
-        //tabWidget = findViewById(R.id.tabWidget);
-        TabWidget row1 = new TabWidget(this);
-        TextView textView1 = new TextView(this);
-        row1.addView(textView1);
-        tableLayout.addView(row1);*/
 
         FloatingActionButton fabPlus = findViewById(R.id.fabPlus);
         fabPlus.setOnClickListener(new View.OnClickListener() {
@@ -60,21 +62,51 @@ public class Summary extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), EditData.class);
                 intent.putExtra(INCOME, "income");
                 startActivity(intent);
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
             }
         });
         FloatingActionButton fabMinus = findViewById(R.id.fabMinus);
         fabMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),EditData.class);
-                intent.putExtra(EXPENSE,"expense");
+                Intent intent = new Intent(getApplicationContext(), EditData.class);
+                intent.putExtra(EXPENSE, "expense");
                 startActivity(intent);
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
+
             }
         });
     }
 
+    private void readJson() {
+
+        try {
+            JSONArray jArray = new JSONArray(readJSONFromAsset());
+            for (int i = 0; i < jArray.length(); ++i) {
+                String name = jArray.getJSONObject(i).getString("name");// name of the country
+                String dial_code = jArray.getJSONObject(i).getString("dial_code"); // dial code of the country
+                String code = jArray.getJSONObject(i).getString("code"); // code of the country
+                ArrayList<String> countryListArray = null;
+                countryListArray.add(dial_code + "  (" + name + ")");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private String readJSONFromAsset() {
+        String json;
+        try {
+            InputStream is = getAssets().open("country_list.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
 }
