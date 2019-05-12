7242052ed5d046caa018d49ce635964cc907e720
Pavan Kumar Kandapagari <40955141+785pavan@users.noreply.github.com>
2019-05-13 01:23:04 +0200

    Username and password saved

--- app/src/main/java/com/dbse/android/spendemon/SignUpLogIn.java
+++ app/src/main/java/com/dbse/android/spendemon/SignUpLogIn.java
@@ -7,7 +7,9 @@ package com.dbse.android.spendemon;
 
 //import android.support.constraint.ConstraintLayout;
 //import android.support.v7.app.AppCompatActivity;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -25,22 +27,24 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import java.util.Objects;
 
-public class SignUpLogIn extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener{
+public class SignUpLogIn extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
 
+    private static final String TAG = "myTag";
     EditText usernameField;
     EditText passwordField;
     private String usernameA1 = null;
     private Integer passwordA1;
     private String[] usernameArray;
     private Integer[] passwordArray;
-    TextView  changeSignUpModeTextView;
+    TextView changeSignUpModeTextView;
     Boolean signUpModeActive;
     Button signUpButton;
     RelativeLayout relativeLayout;
     ConstraintLayout constraintLayout;
     ImageView teamLogo;
+    SharedPreferences sharedPreferences;
 
-    public void signUpOrLogIn(View view){
+    public void signUpOrLogIn(View view) {
         if (signUpModeActive) {
             Log.i("AppInfo", String.valueOf(usernameField.getText()));
             Log.i("AppInfo", String.valueOf(passwordField.getText()));
@@ -50,8 +54,7 @@ public class SignUpLogIn extends AppCompatActivity implements View.OnClickListen
             } else if (String.valueOf(usernameField.getText()).equals("")) {
                 Log.i("Error2", "The Username field should not be empty");
                 Toast.makeText(getApplicationContext(), "The Username field should not be empty", Toast.LENGTH_LONG).show();
-            }
-            else if (String.valueOf(passwordField.getText()).equals("")) {
+            } else if (String.valueOf(passwordField.getText()).equals("")) {
                 Log.i("Error4", "The Password field should not be empty");
                 Toast.makeText(getApplicationContext(), "The Password field should not be empty", Toast.LENGTH_LONG).show();
             }
@@ -61,36 +64,41 @@ public class SignUpLogIn extends AppCompatActivity implements View.OnClickListen
             //        }
             else {
                 Log.i("Successful1", "Successful sign Up!");
-                usernameA1 = String.valueOf(usernameField.getText());
-                passwordA1 = Integer.valueOf(String.valueOf(passwordField.getText()));
+                sharedPreferences.edit().putString("UserName", usernameField.getText().toString()).apply();
+                sharedPreferences.edit().putInt("Password", Integer.parseInt(passwordField.getText().toString())).apply();
+
+
+                /*usernameA1 = String.valueOf(usernameField.getText());
+                passwordA1 = Integer.valueOf(String.valueOf(passwordField.getText()));*/
                 Toast.makeText(getApplicationContext(), "You are signed up as: ".concat(usernameA1), Toast.LENGTH_LONG).show();
             }
-        }else {
+        } else {
 
             if (String.valueOf(usernameField.getText()).equals(usernameA1)) {
-                if (Integer.valueOf(String.valueOf(passwordField.getText())).equals(passwordA1)){
+                if (Integer.valueOf(String.valueOf(passwordField.getText())).equals(passwordA1)) {
                     Toast.makeText(getApplicationContext(), "You are Logged in as: ".concat(usernameA1), Toast.LENGTH_LONG).show();
                     Intent intent1 = new Intent(this, Summary.class);
                     startActivity(intent1);
-                }else{
+                } else {
                     Toast.makeText(getApplicationContext(), "Wrong password!", Toast.LENGTH_LONG).show();
                 }
-            }else{
+            } else {
                 Toast.makeText(getApplicationContext(), "Username was not found", Toast.LENGTH_LONG).show();
             }
 
         }
 
 
-
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        signUpModeActive = true;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signuplogin);
+        sharedPreferences  = this.getSharedPreferences("SignUp", 0);
+        usernameA1 = sharedPreferences.getString("UserName", null);
+        passwordA1 = sharedPreferences.getInt("Password", 0);
+        signUpModeActive = sharedPreferences.getBoolean("SignUpMode", true);
         signUpButton = (Button) findViewById(R.id.buttonSignUp);
         teamLogo = (ImageView) findViewById(R.id.teamLogo);
 //        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout1);
@@ -108,6 +116,9 @@ public class SignUpLogIn extends AppCompatActivity implements View.OnClickListen
 
         usernameField.setOnKeyListener(this);
         passwordField.setOnKeyListener(this);
+        Log.d(TAG, "onCreate: "+ sharedPreferences.getBoolean("SignUpMode",true));
+        Log.d(TAG, "signUpOrLogIn: " + usernameA1);
+        Log.d(TAG, "signUpOrLogIn: " + passwordA1);
 
 
     }
@@ -115,22 +126,23 @@ public class SignUpLogIn extends AppCompatActivity implements View.OnClickListen
     @Override
     public void onClick(View v) {
 
-        if (v.getId() == R.id.changeSignUpMode){
-            if (signUpModeActive){
+        if (v.getId() == R.id.changeSignUpMode) {
+            if (signUpModeActive) {
 
                 signUpModeActive = false;
                 changeSignUpModeTextView.setText(getString(R.string.signUp1));
                 signUpButton.setText(getString(R.string.LogIn1));
-            }else{
+            } else {
 
                 signUpModeActive = true;
                 changeSignUpModeTextView.setText(getString(R.string.LogIn1));
                 signUpButton.setText(getString(R.string.signUp1));
             }
+            sharedPreferences.edit().putBoolean("SignUpMode", signUpModeActive).apply();
             Log.i("AppInfo", "Change the mode");
-        } else if (v.getId() == R.id.teamLogo || v.getId() == R.id.relativeLayout1){
+        } else if (v.getId() == R.id.teamLogo || v.getId() == R.id.relativeLayout1) {
             InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(Objects.requireNonNull(getCurrentFocus()).getWindowToken(),0);
+            imm.hideSoftInputFromWindow(Objects.requireNonNull(getCurrentFocus()).getWindowToken(), 0);
             Log.i("AppInfo", "Clicked on");
         }
 
@@ -141,7 +153,7 @@ public class SignUpLogIn extends AppCompatActivity implements View.OnClickListen
     public boolean onKey(View v, int keyCode, KeyEvent event) {
 //        return false;
 
-        if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP){
+        if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP) {
 //            if (usernameField.getText() == null){
 //                Log.i("Error4", " One of the fields are empty!");
 //            }
