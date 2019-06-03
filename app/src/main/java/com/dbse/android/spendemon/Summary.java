90d7e02ff39ebd1a8b49f813a846ee2f116564d4
Behnam Beladi <42998473+gbehnam@users.noreply.github.com>
2019-06-03 19:28:42 +0200

    edit data

--- app/src/main/java/com/dbse/android/spendemon/Summary.java
+++ app/src/main/java/com/dbse/android/spendemon/Summary.java
@@ -2,7 +2,9 @@ package com.dbse.android.spendemon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -92,6 +94,7 @@ public class Summary extends AppCompatActivity  {
         });
 
     }
+
 
 
 
