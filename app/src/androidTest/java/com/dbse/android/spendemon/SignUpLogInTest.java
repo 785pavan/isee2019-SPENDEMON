4c27a0fb56e2ed9848d938475e44d1fd674f4d89
Pavan Kumar Kandapagari <40955141+785pavan@users.noreply.github.com>
2019-06-11 01:23:11 +0200

    Login class tested

---
+++ app/src/androidTest/java/com/dbse/android/spendemon/SignUpLogInTest.java
@@ -0,0 +1,43 @@
+package com.dbse.android.spendemon;
+
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class SignUpLogInTest {
+
+    @Rule
+    public ActivityTestRule<SignUpLogIn> mActivityTestRule = new ActivityTestRule<>(SignUpLogIn.class);
+
+    private SignUpLogIn mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+
+    }
+
+    @Test
+    public void testLaunchViews() {
+        View editText = mActivity.findViewById(R.id.username1);
+        assertNotNull(editText);
+        editText = mActivity.findViewById(R.id.password1);
+        assertNotNull(editText);
+        View button = mActivity.findViewById(R.id.buttonSignUp);
+        assertNotNull(button);
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+
+    }
+}
