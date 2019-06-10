bdd55facfa18082f84bda7d2827e9cec84a4e3f3
Behnam Beladi <42998473+gbehnam@users.noreply.github.com>
2019-06-10 21:46:35 +0200

    summary navigation bar added

--- app/src/main/java/com/dbse/android/spendemon/Summary.java
+++ app/src/main/java/com/dbse/android/spendemon/Summary.java
@@ -2,12 +2,16 @@ package com.dbse.android.spendemon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,17 +19,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.dbse.android.spendemon.model.Entry;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 
-public class Summary extends AppCompatActivity {
+public class Summary extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "myTag";
     static ArrayList<Entry> entries = new ArrayList<>();
     private final String TYPE = "type";
     private SummaryViewModel summaryViewModel; // object of View Model created.
+    private DrawerLayout drawer1;
 
 
     @Override
@@ -52,8 +58,33 @@ public class Summary extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+//        adding the navigation bar code:
+
+        drawer1 = findViewById(R.id.drawer_layout_sum);
+
+
+        NavigationView navigationView1 = findViewById(R.id.navigation_view1_Sum);
+
+        NavigationView navigationView2 = findViewById(R.id.navigation_view2_Sum);
+
+        navigationView1.setNavigationItemSelectedListener(this);
+
+        navigationView2.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle1 = new ActionBarDrawerToggle(this, drawer1, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer1.addDrawerListener(toggle1);
+        toggle1.syncState();
+
+
+
+
         RecyclerView rvEntries = findViewById(R.id.rvEntries);
         //Button bDelete = findViewById(R.id.bDelete);
 
@@ -97,5 +128,91 @@ public class Summary extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
+        switch (menuItem.getItemId()) {
+            case R.id.nav_daily:
+
+                /*getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new DailyFragment()).commit();*/
+                Intent intent_daily = new Intent(getApplicationContext(), PieChartDailyActivity.class);
+                startActivity(intent_daily);
+                break;
+            case R.id.nav_weekly:
+                Intent intent_month = new Intent(getApplicationContext(), ChartMonthActivity.class);
+                startActivity(intent_month);
+                /*getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new WeeklyFragment()).commit();*/
+                break;
+            case R.id.nav_monthly:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MonthlyFragment()).commit();
+                break;
+            case R.id.nav_total:
+                Intent intent_total = new Intent(getApplicationContext(), PieChartActivity.class);
+                intent_total.putExtra("Duration", "All");
+                startActivity(intent_total);
+                /*getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TotalFragment()).commit();*/
+                break;
+            case R.id.nav_trendLine:
+                Intent intent_trendLine = new Intent(getApplicationContext(), TrendLineActivity.class);
+                startActivity(intent_trendLine);
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_LONG).show();
+                Intent intent_share = new Intent(Intent.ACTION_SEND);
+                intent_share.setType("text/plain");
+                String shareBody = "We are team SPENDEMON. Please check out our blog: https://dbse-teaching.github.io/isee2019-SPENDEMON/";
+                String shareSubject = "The link for our Blog";
+                intent_share.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+                intent_share.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(intent_share, "Share this by"));
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this, "Contact Us E-mail", Toast.LENGTH_LONG).show();
+                Intent intent_contactUs = new Intent(Intent.ACTION_SEND);
+                intent_contactUs.setType("message/rfc822");
+                String contactBody = "Sent from SPENDEMON app.";
+                String contactSubject = "Feedback for SPENDEMON team";
+                intent_contactUs.putExtra(Intent.EXTRA_SUBJECT, contactSubject);
+                intent_contactUs.putExtra(Intent.EXTRA_TEXT, contactBody);
+                intent_contactUs.putExtra(Intent.EXTRA_EMAIL, new String[]{"pavan.kandapagari@st.ovgu.de",
+                        "atrayee.neog@st.ovgu.de", "seyedbehnam.beladi@st.ovgu.de"});
+                startActivity(Intent.createChooser(intent_contactUs, "Share this by"));
+                break;
+//                pavan.kandapagari@st.ovgu.de,atrayee.neog@st.ovgu.de,seyedbehnam.beladi@st.ovgu.de
+            case R.id.nav_summary:
+                Intent intent = new Intent(getApplicationContext(), Summary.class);
+                startActivity(intent);
+                /*getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SummaryFragment()).commit();*/
+//                right drawer
+            case R.id.nav_currency:
+                Toast.makeText(this, "Currency", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_Calculator:
+                Toast.makeText(this, "Calculator", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_settings:
+                Intent intent_set = new Intent(getApplicationContext(),SettingsActivity.class);
+                startActivity(intent_set);
+                break;
+            case R.id.nav_aboutUs:
+                Toast.makeText(this, "About Us", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        drawer1.closeDrawer(GravityCompat.START);
+
+
+        return true;
+    }
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
