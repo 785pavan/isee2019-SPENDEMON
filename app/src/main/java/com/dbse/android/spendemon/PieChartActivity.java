667515a21876b811a97a599853448cd20a7c02bd
Behnam Beladi <42998473+gbehnam@users.noreply.github.com>
2019-06-10 22:42:13 +0200

    pie chart all navigation view

--- app/src/main/java/com/dbse/android/spendemon/PieChartActivity.java
+++ app/src/main/java/com/dbse/android/spendemon/PieChartActivity.java
@@ -4,9 +4,15 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
@@ -17,12 +23,15 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 import static com.dbse.android.spendemon.Summary.entries;
 
-public class PieChartActivity extends AppCompatActivity {
+public class PieChartActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer1;
 
     private static String TAG = "PieChart";
     /* private float[] yData = {25.4f, 10.6f, 66.76f, 44.32f, 46.0f, 16.8f, 23.6f};
@@ -40,9 +49,31 @@ public class PieChartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pie_chart);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer1 = findViewById(R.id.drawer_layout_pie);
+
+
+        NavigationView navigationView1 = findViewById(R.id.navigation_view1_Pie);
+
+        NavigationView navigationView2 = findViewById(R.id.navigation_view2_Pie);
+
+        navigationView1.setNavigationItemSelectedListener(this);
+
+        navigationView2.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle1 = new ActionBarDrawerToggle(this, drawer1, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer1.addDrawerListener(toggle1);
+        toggle1.syncState();
+
 
         intent = getIntent();
-        setContentView(R.layout.activity_pie_chart);
+//        setContentView(R.layout.activity_pie_chart);
         Log.d(TAG, "onCreate: Start of creation of chart");
         pieChart = findViewById(R.id.idPieChart);
         Description desc = new Description();
@@ -193,4 +224,85 @@ public class PieChartActivity extends AppCompatActivity {
 
         }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_daily:
+
+                /*getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new DailyFragment()).commit();*/
+                Intent intent_daily = new Intent(getApplicationContext(), PieChartDailyActivity.class);
+                startActivity(intent_daily);
+                break;
+            case R.id.nav_weekly:
+                Intent intent_month = new Intent(getApplicationContext(), ChartMonthActivity.class);
+                startActivity(intent_month);
+                /*getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new WeeklyFragment()).commit();*/
+                break;
+            case R.id.nav_monthly:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MonthlyFragment()).commit();
+                break;
+            case R.id.nav_total:
+                Intent intent_total = new Intent(getApplicationContext(), PieChartActivity.class);
+                intent_total.putExtra("Duration", "All");
+                startActivity(intent_total);
+                /*getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TotalFragment()).commit();*/
+                break;
+            case R.id.nav_trendLine:
+                Intent intent_trendLine = new Intent(getApplicationContext(), TrendLineActivity.class);
+                startActivity(intent_trendLine);
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_LONG).show();
+                Intent intent_share = new Intent(Intent.ACTION_SEND);
+                intent_share.setType("text/plain");
+                String shareBody = "We are team SPENDEMON. Please check out our blog: https://dbse-teaching.github.io/isee2019-SPENDEMON/";
+                String shareSubject = "The link for our Blog";
+                intent_share.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+                intent_share.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(intent_share, "Share this by"));
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this, "Contact Us E-mail", Toast.LENGTH_LONG).show();
+                Intent intent_contactUs = new Intent(Intent.ACTION_SEND);
+                intent_contactUs.setType("message/rfc822");
+                String contactBody = "Sent from SPENDEMON app.";
+                String contactSubject = "Feedback for SPENDEMON team";
+                intent_contactUs.putExtra(Intent.EXTRA_SUBJECT, contactSubject);
+                intent_contactUs.putExtra(Intent.EXTRA_TEXT, contactBody);
+                intent_contactUs.putExtra(Intent.EXTRA_EMAIL, new String[]{"pavan.kandapagari@st.ovgu.de",
+                        "atrayee.neog@st.ovgu.de", "seyedbehnam.beladi@st.ovgu.de"});
+                startActivity(Intent.createChooser(intent_contactUs, "Share this by"));
+                break;
+//                pavan.kandapagari@st.ovgu.de,atrayee.neog@st.ovgu.de,seyedbehnam.beladi@st.ovgu.de
+            case R.id.nav_summary:
+                Intent intent = new Intent(getApplicationContext(), Summary.class);
+                startActivity(intent);
+                /*getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SummaryFragment()).commit();*/
+//                right drawer
+            case R.id.nav_currency:
+                Toast.makeText(this, "Currency", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_Calculator:
+                Toast.makeText(this, "Calculator", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_settings:
+                Intent intent_set = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent_set);
+                break;
+            case R.id.nav_aboutUs:
+                Toast.makeText(this, "About Us", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        drawer1.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
