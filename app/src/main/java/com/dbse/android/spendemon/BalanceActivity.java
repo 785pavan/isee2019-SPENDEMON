4ce5a29060aba5d4ac1d29b12786603e0e97c51c
Behnam Beladi <42998473+gbehnam@users.noreply.github.com>
2019-06-25 00:02:47 +0200

    Delete the subject for share in drawer

--- app/src/main/java/com/dbse/android/spendemon/BalanceActivity.java
+++ app/src/main/java/com/dbse/android/spendemon/BalanceActivity.java
@@ -203,8 +203,8 @@ public class BalanceActivity extends AppCompatActivity implements NavigationView
                 String shareBody = "We are team SPENDEMON. " +
                         "\n Please check out our App at:" +
                         "\n https://github.com/DBSE-teaching/isee2019-SPENDEMON/releases/download/V0.02/Spendemon.apk";
-                String shareSubject = "The Link to SPENDEMON App:";
-                intent_share.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+//                String shareSubject = "The Link to SPENDEMON App:";
+//                intent_share.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
                 intent_share.putExtra(Intent.EXTRA_TEXT, shareBody);
                 startActivity(Intent.createChooser(intent_share, "Share this by"));
                 break;
