4421005abc7836f7f4bc752b16c60369be692868
Behnam Beladi <42998473+gbehnam@users.noreply.github.com>
2019-06-25 13:37:07 +0200

    new apk address

--- app/src/main/java/com/dbse/android/spendemon/ChartMonthActivity.java
+++ app/src/main/java/com/dbse/android/spendemon/ChartMonthActivity.java
@@ -103,7 +103,7 @@ public class ChartMonthActivity extends AppCompatActivity implements NavigationV
                 intent_share.setType("text/plain");
                 String shareBody = "We are team SPENDEMON. " +
                         "\n Please check out our App at:" +
-                        "\n https://github.com/DBSE-teaching/isee2019-SPENDEMON/releases/download/V0.02/Spendemon.apk";
+                        "\n https://github.com/DBSE-teaching/isee2019-SPENDEMON/releases/download/V0.1.2/Spendemon.apk";
 //                String shareSubject = "The Link to SPENDEMON App:";
 //                intent_share.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
                 intent_share.putExtra(Intent.EXTRA_TEXT, shareBody);
