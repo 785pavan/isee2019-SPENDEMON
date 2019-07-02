34bc5d63698599684baecdc72dd7517b2cb0486e
Pavan Kumar Kandapagari <40955141+785pavan@users.noreply.github.com>
2019-07-02 16:03:41 +0200

    link updated

--- app/src/main/java/com/dbse/android/spendemon/PieChartActivity.java
+++ app/src/main/java/com/dbse/android/spendemon/PieChartActivity.java
@@ -524,7 +524,7 @@ public class PieChartActivity extends AppCompatActivity implements NavigationVie
                 intent_share.setType("text/plain");
                 String shareBody = "We are team SPENDEMON. " +
                         "\n Please check out our App at:" +
-                        "\n https://drive.google.com/open?id=16zQbipnBuJzx8xrgB3kw2iNcvpyBk0Ct";
+                        "\n https://github.com/DBSE-teaching/isee2019-SPENDEMON/releases/download/V0.1.2/Spendemon.apk";
 //                String shareSubject = "The Link to SPENDEMON App:";
 //                intent_share.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
                 intent_share.putExtra(Intent.EXTRA_TEXT, shareBody);
