ee90ae120a2c204e326aee8f5f5e769f9aefaa5f
Pavan Kumar Kandapagari <40955141+785pavan@users.noreply.github.com>
2019-07-02 16:00:59 +0200

    link updated

--- app/src/main/java/com/dbse/android/spendemon/Summary.java
+++ app/src/main/java/com/dbse/android/spendemon/Summary.java
@@ -315,7 +315,7 @@ public class Summary extends AppCompatActivity implements NavigationView.OnNavig
                 intent_share.setType("text/plain");
                 String shareBody = "We are team SPENDEMON. " +
                         "\n Please check out our App at:" +
-                        "\n https://github.com/DBSE-teaching/isee2019-SPENDEMON/releases/download/V0.1.2/Spendemon.apk";
+                        "\n https://drive.google.com/open?id=16zQbipnBuJzx8xrgB3kw2iNcvpyBk0Ct";
 //                String shareSubject = "The Link to SPENDEMON App:";
 //                intent_share.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
                 intent_share.putExtra(Intent.EXTRA_TEXT, shareBody);
